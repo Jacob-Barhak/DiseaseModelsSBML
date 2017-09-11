9493e8053cecef558596b6a172a9ff42bce5609a
leandrohw <leandrohw@gmail.com>
2017-09-11 14:29:16 -0600

    Added R scripts for plotting

---
+++ docs/iBioSim_files/scripts/plot.R
@@ -0,0 +1,19 @@
+#*******************************************************************************
+#  
+# This file is part of iBioSim. Please visit <http://www.async.ece.utah.edu/ibiosim>
+# for the latest version of iBioSim.
+#
+# Copyright (C) 2017 University of Utah
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the Apache License. A copy of the license agreement is provided
+# in the file named "LICENSE.txt" included with this software distribution
+# and also available online at <http://www.async.ece.utah.edu/ibiosim/License>.
+#  
+#*******************************************************************************
+
+source("plot01.R")
+source("plot02.R")
+source("plot03.R")
+source("plot04.R")
+source("plot05.R")
